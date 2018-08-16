8d191be0e45e00f160f4b75e6a28b2316318a40f
Timur Saidov <rapidman.stv@gmail.com>
2018-08-16 20:55:42 +0700

    add script

--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join('/dist/index.html'));
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
